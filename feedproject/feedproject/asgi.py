a91a38c9f3c455f51ce973c509c891f5fa304ad4
julienLombard <julienlombard.fr@gmail.com>
2022-08-07 14:01:10 +0200

    django installation

---
+++ feedproject/feedproject/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for feedproject project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'feedproject.settings')
+
+application = get_asgi_application()
